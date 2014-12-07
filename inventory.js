15d80ae056148b4936cf7960c52f05af1619f777
Spyros Karidis <spyrology@gmail.com>
2014-12-07 16:02:31 -0500

    Add partials and edit item

--- inventory.js
+++ inventory.js
@@ -1,5 +1,53 @@
-var items = [{name: 'Couch', description: 'Ikea this is comfortable!'}, {name: 'Laptop', description: 'Beep boop, goes the laptopt as I create node awesomeness all day long!'}];
+var items = [];
+var _ = require('lodash');
+
+/*
+function findOne(req) {
+	return _.find(items, {id: req.params.id});
+};
+*/
 
 exports.list = function (req, res) {
 	res.render('index', {items: items});
+};
+
+exports.show = function (req, res) {
+	var item = _.find(items, {id: req.params.id});
+	res.render('show', item);
+	console.log("showing");
+};
+
+exports.new = function (req, res) {
+	res.render('new');
+};
+
+exports.create = function (req, res) {
+    var item = {
+        id: _.uniqueId,
+        name: req.body.name,
+        description: req.body.description
+    };
+
+    items.push(item);
+    res.redirect('/');
+    console.log("created");
+};
+
+exports.edit = function (req, res) {
+	var item = _.find(items, {id: req.params.id});
+	res.render('edit', item);
+};
+
+exports.update = function (req, res) {
+	var id = req.params.id;
+	var index = _.findIndex(items, {id: id});
+	var item = {
+		id: id,
+		name: req.body.name,
+		description: req.body.description
+	};
+
+	items[index] = item;
+	console.log('made it');
+	res.redirect('/' + id);
 };
